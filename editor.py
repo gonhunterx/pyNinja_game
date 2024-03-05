0cf7b2aa51d6f71be9769d00ec8abd2695435de1
Jadon <47489511+gonhunterx@users.noreply.github.com>
2024-03-04 22:11:42 -0500

    update

--- editor.py
+++ editor.py
@@ -28,6 +28,10 @@ class Editor:
         self.movement = [False, False, False, False]
         
         self.tilemap = Tilemap(self, tile_size=16)
+        try:
+            self.tilemap.load('map.json')
+        except FileNotFoundError:
+            pass
         
         self.scroll = [0, 0]
         
@@ -121,6 +125,10 @@ class Editor:
                         self.movement[3] = True 
                     if event.key == pygame.K_g:
                         self.ongrid = not self.ongrid
+                    if event.key == pygame.K_o:
+                        self.tilemap.save('map.json')
+                    if event.key == pygame.K_t:
+                        self.tilemap.autotile()
                     if event.key == pygame.K_LSHIFT:
                         self.shift = True 
                     
